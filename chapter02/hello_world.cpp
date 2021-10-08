bc77b86c50eec06c1feb48590acc1edd3174c6ab
Poroszlay Richárd <richardporoszlay@gmail.com>
2021-10-08 13:38:54 +0200

    first_drill módosítása chapter02 névre

---
+++ chapter02/hello_world.cpp
@@ -0,0 +1,8 @@
+//Első Drillem
+#include "std_lib_facilities.h"
+
+int main() {
+	cout << "Hello, World!" << endl;  //A cout a kiírásért felelős, az endl pedig új sort fog beszúrni a program lefutása után. (Megegyezik a \n-el)
+	
+	return 0;
+}
